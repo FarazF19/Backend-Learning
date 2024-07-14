4cde4b552f3e876bfded21288c0909ac384044dd
FarazF19 <mfaraz.bees19@iba-suk.edu.pk>
2024-07-14 21:03:47 +0500

    Learnt Http methods and Url handling

--- file.js
+++ file.js
@@ -4,14 +4,14 @@
 const fs = require("fs");
 
 //write file: Sync
-//fs.writeFileSync("./test.txt", "Hey Faraz");
+fs.writeFileSync("./test.txt", "Hey Faraz");
 
 //write file: Async
-//fs.writeFile("./test.txt", "Hey Faraz Async",(err)=>{})
+fs.writeFile("./test.txt", "Hey Faraz Async",(err)=>{})
 
 //Read File : Sync
-//const readResult = fs.readFileSync('./contacts.txt', "utf8");
-//console.log(readResult);
+const readResult = fs.readFileSync('./contacts.txt', "utf8");
+console.log(readResult);
 
 //append file
 fs.appendFileSync('./test.txt',`${Date.now()} Hey there\n`);
--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hello");
+//Building Http server
+const http = require('http');
+const fs = require('fs');
+
+const myServer = http.createServer((req,res)=>{
+    if(req.url==="/favicon.ico") return res.end();
+    const log = `${Date.now()} : ${req.method} ${req.url} Hey there\n`
+    fs.appendFile('log.txt',log, (err,data)=>{
+        switch (req.url){
+            case "/":
+            if(req.method==='GET') return  res.end('Homepage')
+            break;
+            case "/about":
+                res.end('Iam Faraz');
+            break;
+            case '/signup':
+            if(req.method==="GET") res.end("This is a Signup Form");
+            else if(req.method==='POST'){
+                //DB Query
+                res.end('Success')
+            }
+            default: 
+                res.end('404');
+        }
+    })
+    
+})
+
+myServer.listen(8000,()=>console.log("Server Started!"));
