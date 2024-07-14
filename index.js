e882c3ffee8266306abe20ce5846553fc54b2235
FarazF19 <mfaraz.bees19@iba-suk.edu.pk>
2024-07-14 23:04:08 +0500

    Learnt Express L1

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
 //Building Http server
-const http = require('http');
-const fs = require('fs');
+// const http = require('http');
+// const fs = require('fs');
 
-const myServer = http.createServer((req,res)=>{
-    if(req.url==="/favicon.ico") return res.end();
-    const log = `${Date.now()} : ${req.method} ${req.url} Hey there\n`
-    fs.appendFile('log.txt',log, (err,data)=>{
-        switch (req.url){
-            case "/":
-            if(req.method==='GET') return  res.end('Homepage')
-            break;
-            case "/about":
-                res.end('Iam Faraz');
-            break;
-            case '/signup':
-            if(req.method==="GET") res.end("This is a Signup Form");
-            else if(req.method==='POST'){
-                //DB Query
-                res.end('Success')
-            }
-            default: 
-                res.end('404');
-        }
-    })
+// const myServer = http.createServer((req,res)=>{
+//     if(req.url==="/favicon.ico") return res.end();
+//     const log = `${Date.now()} : ${req.method} ${req.url} Hey there\n`
+//     fs.appendFile('log.txt',log, (err,data)=>{
+//         switch (req.url){
+//             case "/":
+//             if(req.method==='GET') return  res.end('Homepage')
+//             break;
+//             case "/about":
+//                 res.end('Iam Faraz');
+//             break;
+//             case '/signup':
+//             if(req.method==="GET") res.end("This is a Signup Form");
+//             else if(req.method==='POST'){
+//                 //DB
+//                 res.end('Success')
+//             }
+//             default: 
+//                 res.end('404')
+//         }
+//     })
     
+// })
+
+// myServer.listen(8000,()=>console.log("Server Started!"));
+
+//Express Server: Makes it easy with builtin handlers for get . post etc and http 
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send("Hello from Homepage")
+});
+
+app.get('/about',(req,res)=>{
+    res.send(`Hello ${req.query.name}`)
 })
 
-myServer.listen(8000,()=>console.log("Server Started!"));
+app.listen(8000,()=>console.log('Server Started!'));
